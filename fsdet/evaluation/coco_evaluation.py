2757af9d3bb9e370cac1e8b1b1021733c7454623
Jonas Meier <meierjonas96@web.de>
2021-04-14 13:03:17 +0200

    Add functionality for printing per-class AP50. Add an extra line break to each metric print message to improve readability.

--- fsdet/evaluation/coco_evaluation.py
+++ fsdet/evaluation/coco_evaluation.py
@@ -218,11 +218,51 @@ class COCOEvaluator(DatasetEvaluator):
         """
         def log_info_and_append(file, str):
             with open(file, 'a') as f:
-                f.write(str + '\n')
+                f.write(str + 2*'\n')
             self._logger.info(str)
 
+        def get_per_category_ap_table(coco_eval, class_names, iou_low=0.5, iou_high=0.95):
+            # from https://github.com/facebookresearch/Detectron/blob/a6a835f5b8208c45d0dce217ce9bbda915f44df7/detectron/datasets/json_dataset_evaluator.py#L222-L252 # noqa
+            def _get_thr_ind(coco_eval, thr):
+                ind = np.where((coco_eval.params.iouThrs > thr - 1e-5) &
+                               (coco_eval.params.iouThrs < thr + 1e-5))[0][0]
+                iou_thr = coco_eval.params.iouThrs[ind]
+                assert np.isclose(iou_thr, thr)
+                return ind
+            precisions = coco_eval.eval["precision"]
+            # precision has dims (iou, recall, cls, area range, max dets)
+            assert len(class_names) == precisions.shape[2]
+            ind_lo = _get_thr_ind(coco_eval, iou_low)
+            ind_hi = _get_thr_ind(coco_eval, iou_high)
+            results_per_category = []
+            for idx, name in enumerate(class_names):
+                # area range index 0: all area ranges
+                # max dets index -1: typically 100 per image
+                precision = precisions[ind_lo:(ind_hi + 1), :, idx, 0, -1]
+                precision = precision[precision > -1]
+                ap = np.mean(precision) if precision.size else float("nan")
+                results_per_category.append(("{}".format(name), float(ap * 100)))
+            # tabulate it
+            N_COLS = min(6, len(results_per_category) * 2)
+            results_flatten = list(itertools.chain(*results_per_category))
+            results_2d = itertools.zip_longest(
+                *[results_flatten[i::N_COLS] for i in range(N_COLS)])
+            if iou_low == 0.5 and iou_high == 0.95:
+                ap_name = "AP"
+            elif iou_low == iou_high:
+                ap_name = "AP{}".format(round(iou_low * 100))  # e.g. AP50
+            else:  # very rare case
+                ap_name = "AP{}-{}".format(round(iou_low * 100), round(iou_high * 100))  # e.g. AP65-80
+            table = tabulate(
+                results_2d,
+                tablefmt="pipe",
+                floatfmt=".3f",
+                headers=["category", ap_name] * (N_COLS // 2),
+                numalign="left",
+            )
+            return results_per_category, table
+
         metrics = ["AP", "AP50", "AP75", "APs", "APm", "APl"]
-        # TODO: think about (also/just) returning per-class mAP@50? (or at least additionally save the mAP50 results)
         if coco_eval is None:
             self._logger.warn("No predictions from the model! Set scores to -1")
             return {metric: -1 for metric in metrics}
@@ -237,37 +277,21 @@ class COCOEvaluator(DatasetEvaluator):
 
         if class_names is None or len(class_names) <= 1:
             return results
-        # Compute per-category AP
-        # from https://github.com/facebookresearch/Detectron/blob/a6a835f5b8208c45d0dce217ce9bbda915f44df7/detectron/datasets/json_dataset_evaluator.py#L222-L252 # noqa
-        precisions = coco_eval.eval["precision"]
-        # precision has dims (iou, recall, cls, area range, max dets)
-        assert len(class_names) == precisions.shape[2]
 
-        results_per_category = []
-        for idx, name in enumerate(class_names):
-            # area range index 0: all area ranges
-            # max dets index -1: typically 100 per image
-            precision = precisions[:, :, idx, 0, -1]
-            precision = precision[precision > -1]
-            ap = np.mean(precision) if precision.size else float("nan")
-            results_per_category.append(("{}".format(name), float(ap * 100)))
-
-        # tabulate it
-        N_COLS = min(6, len(results_per_category) * 2)
-        results_flatten = list(itertools.chain(*results_per_category))
-        results_2d = itertools.zip_longest(
-            *[results_flatten[i::N_COLS] for i in range(N_COLS)])
-        table = tabulate(
-            results_2d,
-            tablefmt="pipe",
-            floatfmt=".3f",
-            headers=["category", "AP"] * (N_COLS // 2),
-            numalign="left",
-        )
+        # get per-class AP@0.5:0.95, log result, append to file and update 'results'
+        results_per_category, table = get_per_category_ap_table(coco_eval, class_names, iou_low=0.5, iou_high=0.95)
         tmp_str = "Per-category {} AP: \n".format(iou_type) + table
         log_info_and_append(self._summary_file, tmp_str)
-
         results.update({"AP-" + name: ap for name, ap in results_per_category})
+
+        # get per-class AP@0.5, log result, append to file and update 'results'
+        # TODO: probably just do this if 'class_names' are all class names
+        #  -> What's with the case of base training? We then wouldn't have any per-class AP@50 results
+        results_per_category, table = get_per_category_ap_table(coco_eval, class_names, iou_low=0.5, iou_high=0.5)
+        tmp_str = "Per-category {} AP50: \n".format(iou_type) + table
+        log_info_and_append(self._summary_file, tmp_str)
+        results.update({"AP@0.5-" + name: ap for name, ap in results_per_category})  # TODO: necessary for AP@0.5?
+
         return results
 
 
