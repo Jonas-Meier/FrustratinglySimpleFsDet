391c4794e08bd234776aa2e814f22de93165cb83
Jonas Meier <meierjonas96@web.de>
2021-04-16 17:09:22 +0200

    Add num-threads as parameter. Re-arrange variables.

--- wrapper_base_training.py
+++ wrapper_base_training.py
@@ -6,25 +6,25 @@ def main():
     coco_class_split = "voc_nonvoc"  # voc_nonvoc, none_all
     isaid_class_split = "vehicle_nonvehicle"  # vehicle_nonvehicle, none_all, experiment1, experiment2, experiment3
     gpu_ids = [0]
-    layers = 50  # 50, 101
+    num_threads = 1
     bs = 16
     lr = 0.02  # 0.02 for bs=16. Set to -1 for automatic linear scaling!
+    layers = 50  # 50, 101
     override_config = True
-    # --num-threads=1
     if dataset == "coco":
         class_split = coco_class_split
     elif dataset == "isaid":
         class_split = isaid_class_split
     else:
         raise ValueError("Unknown dataset: {}".format(dataset))
-    run_base_training(dataset, class_split, gpu_ids, layers, bs, lr, override_config)
+    run_base_training(dataset, class_split, gpu_ids, num_threads, layers, bs, lr, override_config)
 
 
-def run_base_training(dataset, class_split, gpu_ids, layers, bs, lr=-1.0, override_config=False):
+def run_base_training(dataset, class_split, gpu_ids, num_threads, layers, bs, lr=-1.0, override_config=False):
     base_cmd = "python3 -m tools.run_base_training"
     override_config_str = ' --override-config' if override_config else ''
-    cmd = "{} --dataset {} --class-split {} --gpu-ids {} --layers {} --bs {} --lr {}{}"\
-        .format(base_cmd, dataset, class_split, comma_sep(gpu_ids), layers, bs, lr, override_config_str)
+    cmd = "{} --dataset {} --class-split {} --gpu-ids {} --num-threads {} --layers {} --bs {} --lr {}{}"\
+        .format(base_cmd, dataset, class_split, comma_sep(gpu_ids), num_threads, layers, bs, lr, override_config_str)
     os.system(cmd)
 
 
--- wrapper_fine_tuning.py
+++ wrapper_fine_tuning.py
@@ -6,28 +6,28 @@ def main():
     coco_class_split = "voc_nonvoc"  # voc_nonvoc, none_all
     isaid_class_split = "vehicle_nonvehicle"  # vehicle_nonvehicle, none_all, experiment1, experiment2, experiment3
     gpu_ids = [0]
-    layers = 50  # 50, 101
+    num_threads = 1
     bs = 16
     lr = 0.001  # 0.001 for bs=16. Set to -1 for automatic linear scaling!
     shots = [10]
     seeds = [0]  # single seed or two seeds representing a range, 2nd argument inclusive!
+    layers = 50  # 50, 101
     classifier = 'fc'  # fc, cosine
     tfa = False  # False: randinit surgery
     unfreeze = False  # False: freeze feature extractor while fine-tuning
     override_config = True
     override_surgery = True
-    # --num-threads=1
     if dataset == "coco":
         class_split = coco_class_split
     elif dataset == "isaid":
         class_split = isaid_class_split
     else:
         raise ValueError("Unknown dataset: {}".format(dataset))
-    run_fine_tuning(dataset, class_split, shots, seeds, gpu_ids, layers, bs,
+    run_fine_tuning(dataset, class_split, shots, seeds, gpu_ids, num_threads, layers, bs,
                     tfa, unfreeze, classifier, lr,  override_config, override_surgery)
 
 
-def run_fine_tuning(dataset, class_split, shots, seeds, gpu_ids, layers, bs,
+def run_fine_tuning(dataset, class_split, shots, seeds, gpu_ids, num_threads, layers, bs,
                     tfa=False, unfreeze=False, classifier='fc', lr=-1.0, override_config=False, override_surgery=False):
     base_cmd = "python3 -m tools.run_experiments"
     tfa_str = ' --tfa' if tfa else ''
@@ -35,9 +35,9 @@ def run_fine_tuning(dataset, class_split, shots, seeds, gpu_ids, layers, bs,
     override_config_str = ' --override-config' if override_config else ''
     override_surgery_str = ' --override-surgery' if override_surgery else ''
     cmd = "{} --dataset {} --class-split {} --shots {} --seeds {} " \
-          "--gpu-ids {} --layers {} --bs {} --lr {} --classifier {}{}{}{}{}"\
+          "--gpu-ids {} --num-threads {} --layers {} --bs {} --lr {} --classifier {}{}{}{}{}"\
         .format(base_cmd, dataset, class_split, separate(shots, ' '), separate(seeds, ' '), separate(gpu_ids, ','),
-                layers, bs, lr, classifier, tfa_str, unfreeze_str, override_config_str, override_surgery_str)
+                num_threads, layers, bs, lr, classifier, tfa_str, unfreeze_str, override_config_str, override_surgery_str)
     os.system(cmd)
 
 
