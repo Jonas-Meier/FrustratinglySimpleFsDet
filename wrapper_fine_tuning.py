ae07ade1651979a2f1d0202ba77b73f683568884
Jonas Meier <meierjonas96@web.de>
2021-05-27 19:47:56 +0200

    Set num threads to two because it's a bit faster than just one thread (possibly easier to bypass io-wait) and as fast as four threads (possibly due to less inter-process communication).

--- wrapper_fine_tuning.py
+++ wrapper_fine_tuning.py
@@ -6,7 +6,7 @@ def main():
     coco_class_split = "voc_nonvoc"  # voc_nonvoc, none_all
     isaid_class_split = "vehicle_nonvehicle"  # vehicle_nonvehicle, none_all, experiment1, experiment2, experiment3
     gpu_ids = [0]
-    num_threads = 4  # four threads seem to be a bit faster than just one
+    num_threads = 2  # two threads seem to be a bit faster than just one, but four threads are as fast as two threads!
     bs = 16
     lr = 0.001  # 0.001 for bs=16. Set to -1 for automatic linear scaling!
     shots = [10]
--- wrapper_inference.py
+++ wrapper_inference.py
@@ -8,7 +8,7 @@ def main():
     coco_class_split = "voc_nonvoc"  # voc_nonvoc, none_all
     isaid_class_split = "vehicle_nonvehicle"  # vehicle_nonvehicle, none_all, experiment1, experiment2, experiment3
     gpu_ids = [0]
-    num_threads = 1
+    num_threads = 2
     phase = 2  # phase 1: base-training, phase 2: fine-tuning
     #bs = 16
     shots = [10]
