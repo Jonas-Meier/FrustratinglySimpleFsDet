3e98c57564f5d7dfebecf32345d5d6f88415c317
Jonas Meier <meierjonas96@web.de>
2021-11-04 13:43:51 +0100

    Fix small issue where already existent data would be deleted if data would be sampled for two different shots (for same dataset, class split and seed) but with two independent calls.

--- datasets/prepare_coco_few_shot.py
+++ datasets/prepare_coco_few_shot.py
@@ -23,6 +23,8 @@ def parse_args():
                              "argument being inclusive as well!")
     parser.add_argument("--no-shuffle", action="store_false", default=True, dest="shuffle",
                         help="Shuffle images prior to sampling of annotations.")
+    parser.add_argument("--override", action="store_true", default=False, dest="override",
+                        help="Force deleting already existent data.")
     args = parser.parse_args()
     return args
 
@@ -74,18 +76,45 @@ def generate_seeds(args):
     else:
         assert len(args.seeds) == 2
         seeds = range(args.seeds[0], args.seeds[1] + 1)
+    shots = args.shots
     for i in seeds:
         print("Generating seed {}".format(i))
         save_dir = os.path.join(
             cfg.DATA_SAVE_PATH_PATTERN[args.dataset].format(args.class_split),
             'seed{}'.format(i)
         )
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        else:
-            print("Cleaning save path directory '{}'".format(save_dir))
-            shutil.rmtree(save_dir)
-            os.mkdir(save_dir)
+
+        if os.path.exists(save_dir):
+            if args.override:
+                print("Cleaning save path directory '{}'".format(save_dir))
+                shutil.rmtree(save_dir)
+                os.mkdir(save_dir)
+            else:
+                train_split = cfg.TRAIN_SPLIT[args.dataset]
+                # Note: the "incompatible" annotation file originates from 'tools/examine_sampling_strategies.py' which
+                #  creates just a single annotation file containing annotations for all classes rather than this script
+                #  which creates a single annotation file per class. Since 'fsdet/data/meta_coco.py' searches for
+                #  this "incompatible" annotation file, both types may not exist in the same directory. For that reason,
+                #  we expect it to not being present (or deleted first) prior to creating annotation files with this
+                #  script!
+                incomp_anno_pattern = os.path.join(save_dir, "full_box_{}shot_{}.json")
+                cls_anno_pattern = os.path.join(save_dir, "full_box_{}shot_{}_{}.json")
+                abort = False
+                if any(os.path.exists(incomp_anno_pattern.format(shot, train_split)) for shot in shots):
+                    print("Error: An incompatible annotation file '{}' already exists for a given target shot {}. "
+                          "Set '--override' to delete this directory!".format(incomp_anno_pattern, shots))
+                    abort = True
+                if any(os.path.exists(cls_anno_pattern.format(shot, cat, train_split)) for shot in shots for cat in all_classes):
+                    print("Error: Some data matching the file name '{}' would be overridden by this call for the"
+                          "given shots {} and categories {}. Adjust the arguments or set '--override' to delete this "
+                          "directory!".format(cls_anno_pattern, shots, all_classes))
+                    abort = True
+                if abort:
+                    exit(1)
+                del train_split
+                del incomp_anno_pattern
+                del cls_anno_pattern
+        os.makedirs(save_dir, exist_ok=True)
 
         for cat_name in all_classes:
             print("Generating data for class {}".format(cat_name))
@@ -96,29 +125,29 @@ def generate_seeds(args):
                 else:
                     img_id_to_annos[anno['image_id']] = [anno]
 
-            for shots in args.shots:
+            for shot in shots:
                 sample_annos = []  # annotations
                 sample_imgs = []  # images
                 sample_img_ids = []  # ids of sampled images, just used for duplicate checks
                 if cat_name in base_classes:
                     assert cat_name not in novel_classes
                     if cfg.BASE_SHOT_MULTIPLIER == -1:
-                        target_shots = len(cat_name_to_annos[cat_name])  # should be all available annos
+                        target_shot = len(cat_name_to_annos[cat_name])  # should be all available annos
                         print("Using all available {} annotations for base class {}!"
-                              .format(target_shots, cat_name))
+                              .format(target_shot, cat_name))
                     else:
                         assert cfg.BASE_SHOT_MULTIPLIER > 0
-                        target_shots = cfg.BASE_SHOT_MULTIPLIER * shots
+                        target_shot = cfg.BASE_SHOT_MULTIPLIER * shot
                         print("Generating {}x{} shot data for base class {}"
-                              .format(cfg.BASE_SHOT_MULTIPLIER, shots, cat_name))
+                              .format(cfg.BASE_SHOT_MULTIPLIER, shot, cat_name))
                 else:
                     assert cat_name in novel_classes
-                    target_shots = shots
+                    target_shot = shot
                     print("Generating {} shot data for novel class {}"
-                          .format(shots, cat_name))
+                          .format(shot, cat_name))
                 img_ids = list(img_id_to_annos.keys())
                 # while True:
-                    # img_ids = random.sample(list(img_id_to_annos.keys()), shots)
+                    # img_ids = random.sample(list(img_id_to_annos.keys()), shot)
                 # TODO: probably use random.sample(img_ids, 1) in a 'while True'-loop?
                 if args.shuffle:
                     shuffle_seed = i  # Same order for same seeds, but should not matter...
@@ -130,9 +159,9 @@ def generate_seeds(args):
                 for img_id in img_ids:
                     if img_id in sample_img_ids:  # only necessary if we iterate multiple times through all images
                         continue
-                    if len(img_id_to_annos[img_id]) + len(sample_annos) > target_shots:
+                    if len(img_id_to_annos[img_id]) + len(sample_annos) > target_shot:
                         # TODO: This condition may lead to following:
-                        #  1. For k=5 shots and if each image had exactly 2 annotations per class we finally only
+                        #  1. For k=5 shot and if each image had exactly 2 annotations per class we finally only
                         #  have four annotations for that class -> probably too few annotations
                         #  2. In contrast to other approaches, they allow for taking multiple annotations from the
                         #  same image (even more: they only want ALL annotations from an image (for a certain class)
@@ -143,26 +172,26 @@ def generate_seeds(args):
                     sample_img_ids.append(img_id)
                     assert len(sample_imgs) <= len(sample_annos), \
                         "Error, got {} images but only {} annotations!".format(len(sample_imgs), len(sample_annos))
-                    if len(sample_annos) == target_shots:
+                    if len(sample_annos) == target_shot:
                         break
                 # TODO: Probably convert assertion to a warning.
-                assert len(sample_annos) == target_shots, "Wanted {} shots, but only found {} annotations!"\
-                    .format(target_shots, len(sample_annos))
+                assert len(sample_annos) == target_shot, "Wanted {} shot, but only found {} annotations!"\
+                    .format(target_shot, len(sample_annos))
                 new_data = data.copy()
                 new_data['images'] = sample_imgs
                 new_data['annotations'] = sample_annos
                 new_data['categories'] = new_all_cats
 
-                # Note: even if we sample more annotations for base classes we use the original 'shots' in the file
+                # Note: even if we sample more annotations for base classes we use the original 'shot' in the file
                 # name for clarity!
-                save_file = 'full_box_{}shot_{}_{}.json'.format(shots, cat_name, cfg.TRAIN_SPLIT[args.dataset])
+                save_file = 'full_box_{}shot_{}_{}.json'.format(shot, cat_name, cfg.TRAIN_SPLIT[args.dataset])
                 save_path = os.path.join(save_dir, save_file)
                 with open(save_path, 'w') as f:
                     # json.dump(new_data, f)
                     json.dump(new_data, f, indent=2)  # Easier to check files manually
     end = time.time()
     m, s = divmod(int(end-start), 60)
-    print("Created few-shot data for {} shots and {} seeds in {}m {}s"
+    print("Created few-shot data for {} shot and {} seeds in {}m {}s"
           .format(len(args.shots), len(seeds), m, s))
 
 
